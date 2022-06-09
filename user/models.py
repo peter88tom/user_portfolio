3439a27b9dae188d6df5905967028f73e5929fc4
peter88tom <peter88tom@gmail.com>
2022-06-09 12:55:01 +0200

    Extends User model

---
+++ user/models.py
@@ -0,0 +1,25 @@
+from pyexpat import model
+from statistics import mode
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+
+#
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  home_address =models.CharField(max_length=250, blank=True, default="-")
+  phone_number =  models.CharField(max_length=250, blank=True, default="-")
+  latitude = models.CharField(max_length=250, blank=True, default="-")
+  longitude = models.CharField(max_length=250, blank=True, default="-")
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+  if created:
+    Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+  instance.profile.save()
